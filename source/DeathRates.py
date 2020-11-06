a0838fd686cb26857c9d1906fa7d464dda2d83de
DAbbott <DaleAbbott999@gmail.com>
2020-11-06 03:51:30 -0500

    Add rebinning for DeathRates

--- source/DeathRates.py
+++ source/DeathRates.py
@@ -4,6 +4,28 @@ import matplotlib.pyplot as plt
 
 from OrganizeFrames import organize_frame
 from datetime import date, datetime, timedelta
+from scipy.interpolate import BSpline
+
+def rebin( graph, scale ):
+    if not isinstance(scale, int):
+        print ("WARNING you can only rebin with integers. Exiting.")
+        return 0
+    if scale < 1:
+        print ("WARNING you must rescale by a number >=1. Exiting.")
+        return 0
+    x, y = [], []
+    for cur_bin in range(0,len(graph)-scale+1, scale):
+        #print (cur_bin)
+        ave_x = cur_bin+scale/2
+        x.append(ave_x)
+        print (ave_x)
+        ave_y = 0
+        for y_iter in range(cur_bin, cur_bin+scale+1):
+            ave_y += graph[y_iter]
+        ave_y /= scale
+        y.append( graph[cur_bin])
+
+    return [x,y]
 
 # Take in your desired input directory and output name
 parser = argparse.ArgumentParser(description='Read out death rates due to covid19.')
@@ -65,7 +87,6 @@ for cur_type in death_types:
     ax.set_xlim(frame_result.shape[0], 0)
     ax.grid(True)
     ax.set(title=' '.join([country, cur_type]), xlabel="Time [days]", ylabel="Deaths")
-#    ax.set_xticklabels(l_days)
     plt.savefig( '_'.join([country,cur_type,"Deaths.png"]) )
 
 
