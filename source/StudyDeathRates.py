2be83e2d756fb9559ea00b09167ff273c4475d22
DAbbott <DaleAbbott999@gmail.com>
2020-12-13 12:33:23 -0500

    Overlapping plots work

--- source/StudyDeathRates.py
+++ source/StudyDeathRates.py
@@ -4,38 +4,73 @@ import seaborn as sns
 
 from OrganizeFrames import organize_covid_frame
 
-days_back = 3
+def plot_rates(countries_to_study, time_period, suffix=""):
+    day_gap = 7
+    time_period = 120
+    PU = 1E3 # population units are in thousands
+    #country = args.country
+    country = "US"
+    deaths = {"deaths":{}, "DPC":{}}
+    populations = {}
+    frame_results = {}
+    for country in countries_to_study:
+        #deaths[country] = {"deaths":[], "DPC":[]}
+        deaths["deaths"][country] = []
+        deaths["DPC"][country]    = []
+        for day in range(day_gap, day_gap+time_period):
+            frame = organize_covid_frame(country, day, by="country")
+            if country not in populations:
+                populations[country] = frame['PopTotal']
+            cur_deaths = frame['Deaths'].sum() 
+            deaths["deaths"][country].append( cur_deaths )            
+            deaths["DPC"][country].append( cur_deaths/(populations[country]/1000.0) )
 
-countries_to_study = ["US", "Germany", "France", "Italy", "India", "Brazil", "Japan", "Canada"]
+    frame_results["DPC"] = pd.DataFrame(data=deaths["DPC"])
+    frame_results["DPC"] = frame_results["DPC"].iloc[::-1]
+    print (frame_results["DPC"].head())
 
-# Get all the dataframes
-frames = []
-for country in countries_to_study:
-    frames.append(organize_covid_frame(country, days_back, by="country"))
+    frame_results["deaths"] = pd.DataFrame(data=deaths["deaths"])
+    frame_results["deaths"] = frame_results["deaths"].iloc[::-1]
+    print (frame_results["deaths"].head())
 
-frames = pd.DataFrame(frames)
-
-
-day_gap = 7
-PU = 1E3 # population units are in thousands
-country = args.country
-
-frames = []
-deaths = []
-populations = []
-for day in range(day_gap, day_gap+args.time):
-    frame = organize_frame(country, day, by="country")
-    deaths.append( frame['Deaths'].sum() )
-    populations.append( frame['PopTotal'] )
+    #print (populations)
+    #print (deaths)
 
 
 
+    # Append on deaths per capita
+    #frames["DeathsPerCapita"] = frames.apply(lambda row: row.Deaths/row.PopTotal, axis=1)
+    #average_DeathsPerCapita   = frames["DeathsPerCapita"].agg("mean")
 
-# Append on deaths per capita
-frames["DeathsPerCapita"] = frames.apply(lambda row: row.Deaths/row.PopTotal, axis=1)
-average_DeathsPerCapita   = frames["DeathsPerCapita"].agg("mean")
+    if suffix == "":
+        suffix = ".png"
+    else:
+        suffix = ''.join(["_",suffix,".png"])
+    plots = {}
+    plots["DPC"] = sns.lineplot(data=frame_results["DPC"])
+    plots["DPC"].set_xlim(time_period, 0)
+    plots["DPC"].grid(True)
+    plots["DPC"].set(title='Deaths Per Capita', xlabel="Time [days]", ylabel="Deaths/Capita x1E3")
+    #plots["DPC"].plot()
+    plots["DPC"].figure.savefig("deaths_per_capita"+suffix)
 
-print(frames.head())
+    plots["deaths"] = sns.lineplot(data=frame_results["deaths"])
+    plots["deaths"].set_xlim(time_period, 0)
+    plots["deaths"].grid(True)
+    plots["deaths"].set(title='Total Deaths', xlabel="Time [days]", ylabel="Deaths")
+    #plots["deaths"].plot()
+    plots["deaths"].figure.savefig("deaths"+suffix)
 
-sns.
+
+#######################################
+### Main code starts here #############
+#######################################
+
+random_mix = ["US", "Germany", "France", "Italy", "India", "Brazil", "Japan", "Canada", "Mexico", "Thailand"]
+top_gdppc = ["Switzerland", "Ireland", "Norway", "US", "Singapore", "Denmark", "Australia", "Netherlands", "Sweden", "Austria", "Finland", "Germany"]
+friends = ["Finland", "Georgia", "Italy", "US", "Switzerland"]
+
+#plot_rates(random_mix, 180, suffix="")
+#plot_rates(top_gdppc, 180, suffix="topgdp")
+plot_rates(friends, 180, suffix="friends")
 
