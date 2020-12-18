2a01f8b4e70e6ae1739bc38abd902a9368f136eb
DAbbottPersonal <DaleAbbott999@gmail.com>
2020-12-18 05:17:17 -0500

    Add permutation importance to ML study

--- source/StudyEurope.py
+++ source/StudyEurope.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import eli5
 
+from eli5.sklearn import PermutationImportance
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
@@ -18,18 +20,28 @@ for country in countries_to_study:
 
 frames = pd.DataFrame(frames)
 
+# Append any other fields that may be predictive here
+frames["GDPPerCapita"] = frames.apply(lambda row: float(row.GDP)/row.PopTotal, axis=1)
+
 # Append on deaths per capita
 frames["DeathsPerCapita"] = frames.apply(lambda row: row.Deaths/row.PopTotal, axis=1)
 average_DeathsPerCapita   = frames["DeathsPerCapita"].agg("mean")
 
 target = frames.DeathsPerCapita
 
-features = ["Incidence_Rate", "Case-Fatality_Ratio", 
+print (frames.columns)
+
+# Remove "services" as I suspect it is too correlated to agriculture and industry
+features = ["Incident_Rate", "Case_Fatality_Ratio", 
             "PopDensity", 
-            "GDP", "agriculture", "industry", "services",
-            "prim_edu_fem", "prim_edu_male", "seco_edu_fem", "seco_edu_male", "tert_edu_fem", "tert_edu_male"]
+            "GDPPerCapita", "agriculture", "industry"]
+
+# First pass shows education data has little to no ability to predict death rate!
+#           "prim_edu_fem", "prim_edu_male", "seco_edu_fem", "seco_edu_male", "tert_edu_fem", "tert_edu_male"]
 
 data = frames[ features ] 
+
+print (data)
 
 train_data, validation_data, train_target, validation_target  = train_test_split(data, target, random_state=0)
 
@@ -44,5 +56,7 @@ print("Compared to the average deaths per capita: ")
 print(average_DeathsPerCapita)
 
 
-
+print("Testing permutation importance on the features")
+perm = PermutationImportance(covid_model, random_state=1).fit(validation_data, validation_target)
+print(eli5.format_as_text(eli5.explain_weights(perm, feature_names=features)))
 
