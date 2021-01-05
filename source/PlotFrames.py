486be0b2b537777e88d1d06dc6a83a47b320b8bf
DAbbottPersonal <DaleAbbott999@gmail.com>
2021-01-05 07:48:01 -0500

    StudyDeathRates cleanup

--- source/PlotFrames.py
+++ source/PlotFrames.py
@@ -28,7 +28,6 @@ def plot_rates(countries_to_study, time_period, suffix=""):
     populations = {}
     frame_results = {}
     for country in countries_to_study:
-        #deaths[country] = {"deaths":[], "DPC":[]}
         deaths["deaths"][country] = []
         deaths["DPC"][country]    = []
         for day in range(day_gap, day_gap+time_period):
@@ -41,20 +40,9 @@ def plot_rates(countries_to_study, time_period, suffix=""):
 
     frame_results["DPC"] = pd.DataFrame(data=deaths["DPC"])
     frame_results["DPC"] = frame_results["DPC"].iloc[::-1]
-    print (frame_results["DPC"].head())
 
     frame_results["deaths"] = pd.DataFrame(data=deaths["deaths"])
     frame_results["deaths"] = frame_results["deaths"].iloc[::-1]
-    print (frame_results["deaths"].head())
-
-    #print (populations)
-    #print (deaths)
-
-
-
-    # Append on deaths per capita
-    #frames["DeathsPerCapita"] = frames.apply(lambda row: row.Deaths/row.PopTotal, axis=1)
-    #average_DeathsPerCapita   = frames["DeathsPerCapita"].agg("mean")
 
     if suffix == "":
         suffix = ".png"
@@ -65,7 +53,6 @@ def plot_rates(countries_to_study, time_period, suffix=""):
     plots["DPC"].set_xlim(time_period, 0)
     plots["DPC"].grid(True)
     plots["DPC"].set(title='Deaths Per Capita', xlabel="Time [days]", ylabel="Deaths/Capita x1E3")
-    #plots["DPC"].plot()
     plots["DPC"].figure.savefig("deaths_per_capita"+suffix)
     plt.close()
 
@@ -73,14 +60,12 @@ def plot_rates(countries_to_study, time_period, suffix=""):
     plots["deaths"].set_xlim(time_period, 0)
     plots["deaths"].grid(True)
     plots["deaths"].set(title='Total Deaths', xlabel="Time [days]", ylabel="Deaths")
-    #plots["deaths"].plot()
     plots["deaths"].figure.savefig("deaths"+suffix)
     plt.close()
 
 def plot_rates_by_gdp(countries_to_study, days_ago=7, suffix=""):
     PU = 1E3 # population units are in thousands
     today = date_as_str(day="today", style="EU" )
-    #country = args.country
     country = "US"
     stats = {"GDP":[], "deaths":[], "DPC":[]}
     populations = {}
@@ -100,32 +85,23 @@ def plot_rates_by_gdp(countries_to_study, days_ago=7, suffix=""):
         stats["DPC"].append( cur_deaths/(populations[country]/1000.0) )
 
     frame_results = pd.DataFrame(data=stats, index=countries_to_study)
-    #frame_results = frame_results["DPC"].iloc[::-1]
-    print (frame_results)
 
     plots = {}
     plots["per_capita"] = sns.lmplot(x="GDP", y="DPC", data=frame_results)
-    #plots["per_capita"].set_xlim(time_period, 0)
-    #plots["per_capita"].grid(True)
     plots["per_capita"].set(title=today, xlabel="GDP/population [USD]", ylabel="total COVID deaths/population")
-    #plots["deaths"].plot()
     label_points( frame_results["GDP"], frame_results["DPC"], frame_results.index.to_series(), plt.gca()) 
     plots["per_capita"].savefig("GDP_vs_deaths_percapita"+suffix)
     plt.close()
    
 
     plots["total"] = sns.lmplot(x="GDP", y="deaths", data=frame_results)
-    #plots["per_capita"].set_xlim(time_period, 0)
-    #plots["total"].grid(True)
     plots["total"].set(title=today, xlabel="GDP [USD]", ylabel="deaths")
-    #plots["deaths"].plot()
     label_points( frame_results["GDP"], frame_results["deaths"], frame_results.index.to_series(), plt.gca()) 
     plots["total"].savefig("GDP_vs_deaths"+suffix)
     plt.close()
 
 def plot_latest_trend(countries_to_study, suffix=""):
     def func(x, a, b, c, d, e):
-        #return (a + b*x + c*np.exp(d*x)) 
         return (a + b*x + c*x*x + d*np.exp(e*x))
     colors = color_dict(countries_to_study)
     data_path = '../data/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/'
@@ -136,7 +112,6 @@ def plot_latest_trend(countries_to_study, suffix=""):
     time_period = 14
     extrap_days = 10
     PU = 1E3 # population units are in thousands
-    #country = args.country
     country = "US"
     deaths = {"deaths":{}, "DPC":{}}
     populations = {}
@@ -144,7 +119,6 @@ def plot_latest_trend(countries_to_study, suffix=""):
     if suffix != "":
         suffix = "_"+suffix
     for country in countries_to_study:
-        #deaths[country] = {"deaths":[], "DPC":[]}
         deaths["deaths"][country] = []
         deaths["DPC"][country]    = []
         for day in range(day_gap+time_period, day_gap, -1):
@@ -160,12 +134,10 @@ def plot_latest_trend(countries_to_study, suffix=""):
         try:
             # Try with an exponential first
             popt, pcov = opt.curve_fit(func, time_period_x, deaths["DPC"][country], bounds=([-10,-10,-10,-10,-10],[10,10,10,10,10]))
-            print (popt)
         except RuntimeError:
             # Drop the exponential, virtually to zero and refit the curve
             popt, pcov = opt.curve_fit(func, time_period_x, deaths["DPC"][country], bounds=([-10,-1,-0.001,-0.001,-0.001],[10,1,0.001,0.001,0.001]))
-            print ("Exception in fitting...")
-            print (popt)
+            print ("Exception in fitting, reverting to linear fit...")
 
         plot_period_x = range(-1*time_period, extrap_days, 1)
         plt.plot(plot_period_x, func(plot_period_x, *popt), color=colors[country]['nom'], linestyle='--')
@@ -174,37 +146,6 @@ def plot_latest_trend(countries_to_study, suffix=""):
     plt.xlim([-1*time_period, extrap_days+7])
     plt.savefig("week_projection"+suffix)
     plt.close() 
-
-    #frame_results["DPC"] = pd.DataFrame(data=deaths["DPC"])
-    #frame_results["DPC"] = frame_results["DPC"].iloc[::-1]
-
-    #frame_results["deaths"] = pd.DataFrame(data=deaths["deaths"])
-    #frame_results["deaths"] = frame_results["deaths"].iloc[::-1]
-
-
-
-    # Append on deaths per capita
-    #frames["DeathsPerCapita"] = frames.apply(lambda row: row.Deaths/row.PopTotal, axis=1)
-    #average_DeathsPerCapita   = frames["DeathsPerCapita"].agg("mean")
-
-    #if suffix == "":
-    #    suffix = ".png"
-    #else:
-    #    suffix = ''.join(["_",suffix,".png"])
-    #plots = {}
-    #plots["DPC"] = sns.regplot(data=frame_results["DPC"])
-    #plots["DPC"].set_xlim(time_period, 0)
-    #plots["DPC"].grid(True)
-    #plots["DPC"].set(title='Deaths Per Capita', xlabel="Time [days]", ylabel="Deaths/Capita x1E3")
-    #plots["DPC"].figure.savefig("deaths_per_capita"+suffix)
-    #plt.close()
-
-    #plots["deaths"] = sns.regplot(data=frame_results["deaths"])
-    #plots["deaths"].set_xlim(time_period, 0)
-    #plots["deaths"].grid(True)
-    #plots["deaths"].set(title='Total Deaths', xlabel="Time [days]", ylabel="Deaths")
-    #plots["deaths"].figure.savefig("deaths"+suffix)
-    #plt.close()
 
    
 def color_dict(countries):
